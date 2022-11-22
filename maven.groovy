d517b4ff61c6682c8a4e4a57be710fb124adfd73
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-22 15:34:09 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -14,6 +14,11 @@ stage('Compile') {
     sh 'nohup bash mvnw spring-boot:run &'
   sleep 20
    }
+stage('TestApp') {
+
+ sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+  }
    
   stage ('Sonarqube Analisis) {
     withSonarQubeEnv('Sonar'){
