5ae034949f7173040d6b19fb9b35754500fe64b6
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-22 15:16:47 -0300

    Create maven.groovy

---
+++ maven.groovy
@@ -0,0 +1,41 @@
+def llamarPipeline()
+{
+
+stage('Compile') {
+      sh './mvnw clean compile -e'
+     }
+     stage('Test') {
+      sh './mvnw clean test -e'
+    }
+    stage('Jar') {
+      sh './mvnw clean package -e'
+    }
+  stage('Run') {
+    sh 'nohup bash mvnw spring-boot:run &'
+  sleep 20
+   }
+    stage('TestApp') {
+     STAGE = env.STAGE_NAME
+      sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+  }
+  stage ('Sonarqube Analisis) {
+    withSonarQubeEnv('Sonar'){
+		sh 'mvn clean package sonar:sonar'
+    }
+  }
+    stage('SonarQualityGate') {
+    timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true
+    }
+  }
+    
+stage ('UploadNexus'){
+	nexusPublisher nexusInstanceId: 'nsx01', nexusRepositoryId: 'taller4-clases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/var/jenkins_home/workspace/tallerm4-clases_maven-gradle/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+}     
+
+
+
+return this
+
+
+}
