dc81ea81bed43579c795090aa0e947d492ee2ce8
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-22 16:42:12 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -1,6 +1,17 @@
 def llamarPipeline()
 {
 
+stage('Tag to Master'){
+    STAGE = env.STAGE_NAME
+    figlet "Stage: ${env.STAGE_NAME}"
+
+    def git = new helpers.Git()
+    git.tag(env.GIT_LOCAL_BRANCH)
+
+    println "${env.STAGE_NAME} realizado con exito"
+  }	
+	
+
 stage('Compile') {
       sh './mvnw clean compile -e'
      }
